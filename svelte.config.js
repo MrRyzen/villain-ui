4cb5ba571340bc18bf6eda1c9f25a780477ce6e4
MrRyzen <cristobalmd01@gmail.com>
2025-12-03 04:01:39 -0700

    did not add file LOL

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+export default {
+	// Preprocess with Vite for TypeScript and CSS support
+	preprocess: vitePreprocess(),
+	
+	compilerOptions: {
+		// Generate TypeScript definitions
+		// Component library settings
+	}
+};
