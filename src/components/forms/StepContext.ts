6527c7b9ddd5ed5bd2e054170b764dbaf9731133
MrRyzen <cristobalmd01@gmail.com>
2026-01-18 07:12:13 -0700

    reworking stepper form from scratch

--- src/components/forms/StepContext.ts
+++
@@ -1,8 +0,0 @@
-import type { StepContext } from './Stepper.types';
-import { getContext } from 'svelte';
-
-export const STEPPER_CONTEXT_KEY = Symbol('stepper-form-context');
-
-export function getStepperContext(): StepContext | undefined {
-	return getContext<StepContext>(STEPPER_CONTEXT_KEY);
-}
